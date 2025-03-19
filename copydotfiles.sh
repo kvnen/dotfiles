c21223c4caebf616bb323dd267e8fac4e9344a0d
kvnen <koivunen.anttisakari@gmail.com>
2025-03-19 18:56:31 +0200

    made a copy script and added nvim config

---
+++ copydotfiles.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+SCRIPT_DIR=$(dirname "$0")
+cp -r $HOME/.config/nvim "$SCRIPT_DIR" 
---
+++ nvim/init.lua
@@ -0,0 +1,4 @@
+require("config.lazy")
+require("kvnen")
+vim.opt.relativenumber = true
+vim.opt.number = true

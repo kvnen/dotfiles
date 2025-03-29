4a47be353de3b68b56dcb4eb2753f420758fff83
kvnen <koivunen.anttisakari@gmail.com>
2025-03-29 20:07:56 +0200

    added autopair and some simple stuff in init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,7 +5,7 @@ vim.opt.number = true
 -- the thing where it has a margin of 10 lines on top and bottom
 vim.opt.scrolloff = 10
 
-
+vim.diagnostic.config({ virtual_text = true })
 -- disabling arrow keys
 vim.keymap.set('i', '<left>', '<cmd>echo "n00b"<CR>')
 vim.keymap.set('i', '<right>', '<cmd>echo "n00b"<CR>')
@@ -16,3 +16,15 @@ vim.keymap.set('n', '<left>', '<cmd>echo "n00b"<CR>')
 vim.keymap.set('n', '<right>', '<cmd>echo "n00b"<CR>')
 vim.keymap.set('n', '<up>', '<cmd>echo "n00b"<CR>')
 vim.keymap.set('n', '<down>', '<cmd>echo "n00b"<CR>')
+
+--undo file
+vim.opt.undofile = true
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
